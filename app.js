09c960d1be993f976d73fef5e8f401e469aa0a1a
gmachat <machatgreg@gmail.com>
2021-03-06 18:28:59 -0500

    updated mobile responsiveness, add functionality to homemade chute.js

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 let chute = new chutejs
 
-// testel = document.getElementById("army-vet-photo")
-// console.log(testel, 'before')
-// chute.setNewAttributes(testel, {"class": "fun-army"})
-// chute.addToAttributes(testel, {"class": "fun-army", "id":"updated"})
-// console.log('App.js running')
+
+
+
+// const handleCardClick = (event) => {
+
+// } 
--- chutejs.js
+++ chutejs.js
@@ -1,29 +1,51 @@
 function chutejs() {
-let _this = this
+  let _this = this
 
-//allows user to set new attributes to any number of attributes for an element
-//Used for creation of new elements OVERRIDES previous values.  
-//Best used on newly created elements, if updating, use "addToAttributes"
-this.setNewAttributes = (el, attributeObj) => {
-  for(const [attribute, value] of Object.entries(attributeObj)){
-    el.setAttribute(attribute, value)
-  }
-}
-
-
-//allows user to add any amount of attributes and their values to an element
-//updates and APPENDS these values to the previous values.
-this.addToAttributes = (el, attributeObj) => {
-  for(const [attribute, value] of Object.entries(attributeObj)){
-    let attr_value = el.getAttribute(attribute)
-    if(attr_value){
-      attr_value = `${attr_value} ${value}`
+  //allows user to set new attributes to any number of attributes for an element
+  //Used for creation of new elements OVERRIDES previous values.  
+  //Best used on newly created elements, if updating, use "addToAttributes"
+  this.setNewAttributes = (el, attributeObj) => {
+    for (const [attribute, value] of Object.entries(attributeObj)) {
+      el.setAttribute(attribute, value)
     }
-    el.setAttribute(attribute, attr_value)
   }
-}
 
+  //allows user to add any amount of attributes and their values to an element
+  //updates and APPENDS these values to the previous values.
+  this.addToAttributes = (el, attributeObj) => {
+    for (const [attribute, value] of Object.entries(attributeObj)) {
+      let attrValue = el.getAttribute(attribute)
+      if (attrValue) {
+        attrValue = `${attrValue} ${value}`
+      }
+      el.setAttribute(attribute, attrValue)
+    }
+  }
 
+  //Creates a new element and applies attributes from the attribute object to the class
+  this.createFullElement = (tag, attributeObj) => {
+    const newElement = document.createElement(tag)
+    this.setNewAttributes(newElement, attributeObj)
+    return newElement
+  }
+
+  //removes attribute value specified in attributeObj from the given element
+  //for instance it will remove a specific class from an element
+  this.removeFromAttribute = (el, attributeObj) => {
+    for (const [attribute, value] of Object.entries(attributeObj)) {
+      let attrValue = el.getAttribute(attribute)
+      attrValue = attrValue.replace(value, '').trim()
+      el.setAttribute(attribute, attrValue)
+    }
+  }
+
+//create an HTML node from the string passed in to it
+  this.createContentFromString = (string) => {
+      const tempContainer = document.createElement("div")
+      tempContainer.innerHTML = string
+      let node = container.firstChild
+      return node
+    }
 
 
 
