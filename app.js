eab51eb258784c8a0e1189d30d96ff65ccc232fe
gmachat <machatgreg@gmail.com>
2021-03-16 16:49:04 -0400

    added sticky nav bar when moving past portrait, added slide count to projects, minor bug fixes

--- app.js
+++ app.js
@@ -18,6 +18,9 @@ const setBurgerMenu = () => {
   })
 }
 
+
+
+
 //Handle when a card is clicked to bring oup modal
 const handleCardClick = (event, el) => {
   const modal = chute.createFullElement("div",  {"class": "modal", "id": "modal"})
@@ -29,18 +32,26 @@ const handleCardClick = (event, el) => {
   modal.append(targetCard)
 } 
 
+
+
+
+
 //Select all cards form ths about me section and add the click event listener
 const aboutCards = Array.from(document.getElementsByClassName("card"))
 aboutCards.forEach(el => {el.addEventListener('click', (event) => handleCardClick(event, el))})
 
+
+
 //Set up carousel buttons when screen width is below 1024px
 let arrowTimer
 const showCarouselButtons =  (time) => {
-  console.log(event.target)
   if(arrowTimer){
     clearTimeout(arrowTimer)
   }
   let buttons = document.querySelectorAll(".carousel-button-class")
+  let cardCount = document.getElementById("card-count")
+  cardCount.setAttribute('class', 'card-count-active')
+
   //just takes the first button as they all should have same classes at this point
   if(buttons[0].className.includes('carousel-button-hidden')){
   buttons.forEach(btn => {
@@ -50,19 +61,20 @@ const showCarouselButtons =  (time) => {
   arrowTimer = window.setTimeout(()=> {
       buttons.forEach(btn => {
         chute.setNewAttributes(btn, {"class": "carousel-button-hidden carousel-button-class"})
-        console.log(arrowTimer, 'here')
+        cardCount.setAttribute('class', 'card-count-inactive')
       })
     }, time)
-    console.log(arrowTimer)
-
 }
 
 //Carousel
+let currentPage = 1
 const buildCarousel = () => {
   const carousel = document.querySelector('#project-carousel');
   const wrapper = document.querySelector('.wrapper');
-
 	let projectCards = document.querySelectorAll('.project-card');
+  let cardCount = document.getElementById('card-count')
+  const totalPages = projectCards.length
+  cardCount.innerHTML = `${currentPage}/${totalPages}`
   carousel.addEventListener("mousemove", () => showCarouselButtons(2000))
   carousel.addEventListener("click", () => showCarouselButtons(2000))
 	const projectPrev = document.querySelector('#project-button-prev');
@@ -70,12 +82,54 @@ const buildCarousel = () => {
   projectPrev.addEventListener('click', function() {
 		carousel.insertBefore(projectCards [projectCards.length - 1], projectCards [0]);
 		projectCards  = document.querySelectorAll('.project-card');
+    currentPage -= 1
+    if (currentPage == 0) currentPage = totalPages
+    cardCount.innerHTML = `${currentPage}/${totalPages}`
+
 	});
   projectNext.addEventListener('click', function() {
 		carousel.appendChild(projectCards[0]);
 		projectCards = document.querySelectorAll('.project-card');
+    currentPage += 1
+    console.log(currentPage)
+
+    if (currentPage == 7) currentPage = 1
+    cardCount.innerHTML = `${currentPage}/${totalPages}`
+
 	});
 }
+
+{
+const carousel = document.querySelector('#project-carousel');
+const navBar = document.querySelector('nav');
+const about =document.querySelector('about')
+const cardCount = document.querySelector('#card-count')  
+window.addEventListener('scroll', () => {
+
+  if(navBar.getBoundingClientRect().top <= 0){
+    navBar.style.position = 'fixed'
+    navBar.style.top = '0px'
+  }
+  if(navBar.getBoundingClientRect().bottom <= about.getBoundingClientRect().top){
+    navBar.style.position = 'relative'
+  }
+  if(carousel.getBoundingClientRect().top <= 200){
+    cardCount.setAttribute('class', 'card-count-active')
+    window.setTimeout(() => {
+      cardCount.setAttribute('class', 'card-count-inactive')
+    }, 3000)
+  }
+})
+}
+
+// if(navBar.getBoundingClientRect().top <= 0){
+//   console.log('passed')
+//   navBar.style.position = 'fixed'
+//   navBar.style.top = '0px'
+//   navBar.style.width = '100%'
+// }
+
+
 
 
 
